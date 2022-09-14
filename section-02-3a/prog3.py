8e64f672cac73ca561142e767cf83e36620eb2e6
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-14 11:59:00 -0500

    Scripted auto-commit on change (2022-09-14 11:59:00) by gitwatch.sh

--- section-02-3a/prog3.py
+++ section-02-3a/prog3.py
@@ -2,5 +2,7 @@ x = input("What is first number? ")
 y = input("What is second number? ")
 print("x is", x)
 print("y is", y)
-print("Wait, add should work", 9+7)
+a = 9
+b = 7
+print("Wait, add should work", a+b)
 print("Sum is", x+y)
