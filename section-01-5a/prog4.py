8125bcb0f712bb2292302b0b8d9e5235e5a68242
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-14 14:47:06 -0500

    Scripted auto-commit on change (2022-09-14 14:47:06) by gitwatch.sh

--- section-01-5a/prog4.py
+++ section-01-5a/prog4.py
@@ -1,5 +1,5 @@
-x = input("Give me a number: ")
-y = input("Give me a number: ")
+x = int(input("Give me a number: "))
+y = int(input("Give me a number: "))
 print("Total is", x+y)
 print("What?", 9+7)
 print("abc"+"def")
