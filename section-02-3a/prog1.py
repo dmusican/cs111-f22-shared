24f7745daeeedc321dea5ca6b5022f515e0c9677
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-14 11:45:41 -0500

    Scripted auto-commit on change (2022-09-14 11:45:41) by gitwatch.sh

--- section-02-3a/prog1.py
+++ section-02-3a/prog1.py
@@ -5,4 +5,4 @@ print(1, 2, 3)
 
 print('Let us have a "conversation", shall we?')
 print("Let's have a \"conversation\", shall we?")
-print("A backslash looks like this \ thing")
+print("A backslash and an a looks like this \a thing")
