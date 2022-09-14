cad5b20f484f973eac1328d16767ecd2e83f67d5
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-14 12:00:56 -0500

    Scripted auto-commit on change (2022-09-14 12:00:56) by gitwatch.sh

--- section-02-3a/prog3.py
+++ section-02-3a/prog3.py
@@ -1,7 +1,7 @@
 print("abc" + "def")
 
-x = input("What is first number? ")
-y = input("What is second number? ")
+x = int(input("What is first number? "))
+y = int(input("What is second number? "))
 print("x is", x)
 print("y is", y)
 a = 9
