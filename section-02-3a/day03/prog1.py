8989a6cb716ff18a606396386b327f89990ef851
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-16 11:32:54 -0500

    Scripted auto-commit on change (2022-09-16 11:32:54) by gitwatch.sh

--- section-02-3a/day03/prog1.py
+++ section-02-3a/day03/prog1.py
@@ -2,8 +2,8 @@
 #    print(i)
     
 
-for thingybob in [8, 1, 9, 3]:
-    print(thingybob * 2)
+#for thingybob in [8, 1, 9, 3]:
+#    print(thingybob * 2)
 
 for value in range(6):
     print(value)
