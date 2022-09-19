bfaa373e438996843d9cfb9d3bb5d1164cdfc85d
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-19 14:32:10 -0500

    Scripted auto-commit on change (2022-09-19 14:32:10) by gitwatch.sh

--- section-01-5a/day04/prog2.py
+++ section-01-5a/day04/prog2.py
@@ -5,5 +5,7 @@ window.setBackground('white')
 
 house = Rectangle(Point(100, 250), Point(400, 480))
 house.draw(window)
+house.setFill('blue')
+house.setOutline('red)')
 
 keyPress = input('Press enter: ')
