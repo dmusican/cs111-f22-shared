1565deef978b7d954894520ae8cd8d089759df6f
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-21 11:42:21 -0500

    Scripted auto-commit on change (2022-09-21 11:42:21) by gitwatch.sh

--- section-02-3a/day05/prog01.py
+++ section-02-3a/day05/prog01.py
@@ -5,5 +5,9 @@ windowHeight = 600
 win = GraphWin("Game!", windowWidth, windowHeight)
 win.setBackground('white')
 
+# Have user click in window to draw a ball
+location = win.getMouse()
+print(location.getX(), location.getY())
+
 
 keyEntered = input("Enter something: ")
