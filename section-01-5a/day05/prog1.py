332bc707bce87fb261480fe06eda19e69cc86d57
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-21 14:26:57 -0500

    Scripted auto-commit on change (2022-09-21 14:26:57) by gitwatch.sh

--- section-01-5a/day05/prog1.py
+++ section-01-5a/day05/prog1.py
@@ -7,6 +7,12 @@ win = GraphWin("Game!", windowWidth, windowHeight)
 win.setBackground('white')
 
 location = win.getMouse()
+x = location.getX()
+y = location.getY()
+radius = 30
+circ = Circle(Point(x,y), radius)
+
+
 print(location.getX(), location.getY())
 
 keyPressed = input('Type something: ')
