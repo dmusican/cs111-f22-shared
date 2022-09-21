53b5b5b5ba94caeb2c4161777d560045d98268e6
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-21 14:22:53 -0500

    Scripted auto-commit on change (2022-09-21 14:22:53) by gitwatch.sh

---
+++ section-01-5a/day05/prog1.py
@@ -0,0 +1,9 @@
+# A ball that moves around the screen
+from graphics import *
+
+windowWidth = 500
+windowHeight = 600
+win = GraphWin("Game!", windowWidth, windowHeight)
+win.setBackground('white')
+
+keyPressed = input('Type something: ')
