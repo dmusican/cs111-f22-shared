657b6a4ff79dd6e4db009ec716d6a3ea2a2ccda3
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-21 14:37:09 -0500

    Scripted auto-commit on change (2022-09-21 14:37:09) by gitwatch.sh

---
+++ section-01-5a/day05/prog2.py
@@ -0,0 +1,27 @@
+# A ball that moves around the screen
+from graphics import *
+from time import *
+
+windowWidth = 500
+windowHeight = 600
+win = GraphWin("Game!", windowWidth, windowHeight)
+win.setBackground('white')
+
+location = win.getMouse()
+print(type(location))
+x = location.getX()
+y = location.getY()
+raddddddius = 30  # dumb name to make a point
+
+shiftx = 10
+shifty = 20
+for i in range(100):
+    circ = Circle(Point(x,y), raddddddius)
+    circ.draw(win)
+    sleep(0.1)
+    circ.undraw()
+    x = x + shiftx
+    y = y + shifty
+
+
+keyPressed = input('Type something: ')
