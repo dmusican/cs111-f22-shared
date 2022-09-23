89f90a5af273f63b1960c676de651c7061be92b9
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-23 14:27:42 -0500

    Scripted auto-commit on change (2022-09-23 14:27:42) by gitwatch.sh

--- section-01-5a/day06/prog1.py
+++ section-01-5a/day06/prog1.py
@@ -3,5 +3,6 @@ print("first: ", word[0])
 print("second: ", word[1])
 print("multiple: ", word[1:3])
 print("length:", len(word))
-print("last: ", word[len(word)-1])
+print("last:", word[len(word)-1])
+print("last:", word[-1])
 
