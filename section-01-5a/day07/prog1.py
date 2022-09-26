c1b7896502c95a7e2ba0d2740ddb3669e4b7beaf
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-26 14:33:35 -0500

    Scripted auto-commit on change (2022-09-26 14:33:35) by gitwatch.sh

--- section-01-5a/day07/prog1.py
+++ section-01-5a/day07/prog1.py
@@ -1,6 +1,9 @@
 # Convert everything from lower case to upper
 phrase = 'carleton'
 for theLetter in phrase:
+    unicodeValue = ord(theLetter)
+    updatedValue = unicodeValue - 32
+    updatedLetter = chr(updatedValue)
     print(theLetter)
 
 
