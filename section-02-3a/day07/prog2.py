fcb12edbb7641bda96e9f064bcd9323148bf48fc
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-26 11:42:58 -0500

    Scripted auto-commit on change (2022-09-26 11:42:58) by gitwatch.sh

--- section-02-3a/day07/prog2.py
+++ section-02-3a/day07/prog2.py
@@ -2,9 +2,10 @@
 # (There's an easier way, but I want
 # to show this works)
 phrase = 'carleton'
+shift = 32
 for letter in phrase:
     unicodeValue = ord(letter)
-    newValue = unicodeValue - 32
+    newValue = unicodeValue - shift
     newLetter = chr(newValue)
     print(newLetter, end="")
 print()
