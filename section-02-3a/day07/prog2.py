b07633527338bedd745b0a9afc4bb8f423c6f121
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-26 11:44:25 -0500

    Scripted auto-commit on change (2022-09-26 11:44:25) by gitwatch.sh

--- section-02-3a/day07/prog2.py
+++ section-02-3a/day07/prog2.py
@@ -1,7 +1,7 @@
 # Shift from to lower to upper case
 # (There's an easier way, but I want
 # to show this works)
-phrase = 'carleton@##!'
+phrase = 'carleton[]'
 shift = ord('a') - ord('A')
 for symbol in phrase:
     unicodeValue = ord(symbol)
