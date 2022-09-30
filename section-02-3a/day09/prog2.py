23c48f2bae560fc03dbfd60656e0009241e224d6
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-30 12:22:10 -0500

    Scripted auto-commit on change (2022-09-30 12:22:10) by gitwatch.sh

--- section-02-3a/day09/prog2.py
+++ section-02-3a/day09/prog2.py
@@ -3,6 +3,7 @@ def getLength(phrase):
     total = 0
     for character in phrase:
         total = total + 1
+    print("LENGTH IS", total, "!!!!")
     return total
 
 print("length of dog is", getLength("dog"))
