8ed47a9780c8ebf97504145b5e33a8bd08063d05
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-09-30 12:10:25 -0500

    Scripted auto-commit on change (2022-09-30 12:10:25) by gitwatch.sh

--- section-02-3a/day09/prog1.py
+++ section-02-3a/day09/prog1.py
@@ -5,4 +5,11 @@ def show_year():
     year = full_timestamp[:4]
     print("Here is the year:", year)
 
+def get_year():
+    full_timestamp = str(datetime.now())
+    year = full_timestamp[:4]
+    return year
+
+
+
 show_year()
