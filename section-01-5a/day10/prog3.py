a6b8fd846c30878f413a7addb0ff23d344b3fd02
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-05 14:08:31 -0500

    Scripted auto-commit on change (2022-10-05 14:08:31) by gitwatch.sh

--- section-01-5a/day10/prog3.py
+++ section-01-5a/day10/prog3.py
@@ -2,10 +2,13 @@
 # if the word is not punctuation
 # don't translate
 # if starts with a vowel, leave it there
+
 word = input("What should I translate? ")
 
-if word == "?":
+if word in ["?", "!", ","]:
     output = word
+elif word[0] in ["a", "e", "i", "o", "u"]:
+    output = word + "ay"
 else:
     output = word[1:] + word[0] + "ay"
 
