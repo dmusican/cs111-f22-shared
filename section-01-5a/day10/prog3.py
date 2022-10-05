1d14eec5052b7b348a03f1b04b5a591b7f358043
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-05 14:14:07 -0500

    Scripted auto-commit on change (2022-10-05 14:14:07) by gitwatch.sh

--- section-01-5a/day10/prog3.py
+++ section-01-5a/day10/prog3.py
@@ -7,7 +7,7 @@ word = input("What should I translate? ")
 
 if word in ["?", "!", ","]:
     output = word
-if word[0] in ["a", "e", "i", "o", "u"]:
+elif word[0] in ["a", "e", "i", "o", "u"]:
     output = word + "ay"
 else:
     output = word[1:] + word[0] + "ay"
