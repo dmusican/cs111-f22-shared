92a8bdc5600e49f3d6a90ed83e15d4b0b7ec9f68
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-05 11:40:17 -0500

    Scripted auto-commit on change (2022-10-05 11:40:17) by gitwatch.sh

---
+++ section-02-3a/day10/prog4.py
@@ -0,0 +1,10 @@
+x = int(input("Give me a number: "))
+if x == 1:
+    print("Just one")
+elif x == 3:
+    print("You are three for me")
+    print("yay")
+elif x == 2:
+    print(10*10)
+else:
+    print("Nothing here")
