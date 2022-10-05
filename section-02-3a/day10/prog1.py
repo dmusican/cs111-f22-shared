4f726b1df9c1761abbe7bb37dd613a1eb76d12db
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-05 11:16:45 -0500

    Scripted auto-commit on change (2022-10-05 11:16:45) by gitwatch.sh

--- section-02-3a/day10/prog1.py
+++ section-02-3a/day10/prog1.py
@@ -1,6 +1,6 @@
 # Translation to Piglatin, again
 # only if length is 5
-word = "hello"
+word = input("Give me a word: ")
 if len(word) == 5:
     output = word[1:] + word[0] + "ay"
     print(output)
