023a3e263586e9381315f1048c6db5be84ee4f07
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-05 11:31:43 -0500

    Scripted auto-commit on change (2022-10-05 11:31:43) by gitwatch.sh

--- section-02-3a/day10/prog3.py
+++ section-02-3a/day10/prog3.py
@@ -5,6 +5,8 @@
 word = input("Give me a word: ")
 if word in ['?', '!', ',']:
     output = word
+elif word[0] in ['a', 'e', 'i', 'o', 'u']:
+    output = word + "ay"
 else:
     output = word[1:] + word[0] + "ay"
 
