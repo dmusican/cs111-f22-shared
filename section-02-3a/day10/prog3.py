c192c195a18e6c51173550090164b96a4ae8c0fe
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-05 11:30:37 -0500

    Scripted auto-commit on change (2022-10-05 11:30:37) by gitwatch.sh

---
+++ section-02-3a/day10/prog3.py
@@ -0,0 +1,18 @@
+# Translation to Piglatin, again
+# Only translate if no punctuation,
+# but also if starts with a vowel, leave
+# it there
+word = input("Give me a word: ")
+if word == '?':
+    output = word
+else:
+    output = word[1:] + word[0] + "ay"
+
+#######
+if 3 == 5:
+    print("pigs can fly")
+else:
+    print("pigs cannot fly")
+
+
+print("Result is ", output)
