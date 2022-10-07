23f19630896425e2e66d149c9dc2cb883c181e22
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-07 12:08:31 -0500

    Scripted auto-commit on change (2022-10-07 12:08:31) by gitwatch.sh

---
+++ section-02-3a/day11/prog2.py
@@ -0,0 +1,6 @@
+# indefinite loop
+# Keep asking a question until get the right answer
+response = input("What's the best college? ")
+while response != "carleton":
+    print("Wrong answer, try again.")
+    response = input("Really, what's the best college? ")
