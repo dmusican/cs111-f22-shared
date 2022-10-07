25e15d7329b15362d5dde5d3dd3a2aae6abd0bae
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-07 12:19:03 -0500

    Scripted auto-commit on change (2022-10-07 12:19:03) by gitwatch.sh

--- section-02-3a/day11/prog4.py
+++ section-02-3a/day11/prog4.py
@@ -1,13 +1,11 @@
 # Number guessing game
 secret = 37
-# Priming read
-guess = int(input("What is your guess?"))
+
 while guess != secret:
+    guess = int(input("What is your guess?"))
     if guess < secret:
         print("Too small, friend, try again.")
-        guess = int(input("What is your guess?"))
     elif guess > secret:
         print("Too big, my special friend, go again.")
-        guess = int(input("What is your guess?"))
 
 print("Got it")
