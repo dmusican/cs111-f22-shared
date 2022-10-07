684717322590d6958bc5b61fd23fd01194e735d6
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-07 14:38:19 -0500

    Scripted auto-commit on change (2022-10-07 14:38:19) by gitwatch.sh

--- section-01-5a/day11/prog3.py
+++ section-01-5a/day11/prog3.py
@@ -3,14 +3,11 @@
 
 secret = 37
 
-guess = int(input("What's your guess? "))
-
 while guess != secret:
+    guess = int(input("What's your guess? "))
     if guess < secret:
         print("Too small, my friend, try again.")
-        guess = int(input("What's your guess? "))
     elif guess > secret:
         print("Alas your guess is too mighty. Try again.")
-        guess = int(input("What's your guess? "))
     else:
         print("You got it!")
