a3eff46323c5d23256e45f9a9e7a8f5bc71506cc
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-10 14:38:47 -0500

    Scripted auto-commit on change (2022-10-10 14:38:47) by gitwatch.sh

--- section-01-5a/day12/prog3.py
+++ section-01-5a/day12/prog3.py
@@ -5,6 +5,10 @@ dave = FileImage("dave-selfie.gif")
 dave.draw(win)
 
 for i in range(dave.getNumPixels()):
-    print(dave.getPixel1D(i))
+    pixel = dave.getPixel1D(i)
+
+    if pixel.getRed() > 200 and pixel.getGreen() > 200 \
+       and pixel.getBlue() > 200:
+       dave.setPixel1D(i, Pixel(0,0,0))
 
 input("Look at me...")
