fb4cfd098682b7312b5b63b2000a615d3d92b38c
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-10 11:52:48 -0500

    Scripted auto-commit on change (2022-10-10 11:52:48) by gitwatch.sh

--- section-02-3a/day12/proj4.py
+++ section-02-3a/day12/proj4.py
@@ -4,4 +4,9 @@ win = ImageWin(480, 640, "Me!!!!!")
 dave = FileImage('dave-selfie.gif')
 dave.draw(win)
 
+for i in range(dave.getNumPixels()):
+    print(dave.getPixel1D(i)
+
+
+
 input("Waiting....")
