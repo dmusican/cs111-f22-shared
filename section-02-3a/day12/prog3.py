9d87b601dcbef2ff8672a3aeaa47563fe9156600
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-10 11:38:31 -0500

    Scripted auto-commit on change (2022-10-10 11:38:31) by gitwatch.sh

--- section-02-3a/day12/prog3.py
+++ section-02-3a/day12/prog3.py
@@ -1,4 +1,6 @@
 
 response = input("Favorite color?")
-if response != "blue" and response != "green":
+# if response != "blue" and response != "green":
+# if not (response == blue)  and  not (response == green)
+if not (response == "blue" or response == "green"):
     print("Me too!")
