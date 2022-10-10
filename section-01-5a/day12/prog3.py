3926b76aa36009f2e00bc8e25ae97c7d832545a1
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-10 14:43:49 -0500

    Scripted auto-commit on change (2022-10-10 14:43:49) by gitwatch.sh

--- section-01-5a/day12/prog3.py
+++ section-01-5a/day12/prog3.py
@@ -7,8 +7,10 @@ dave.draw(win)
 
 for i in range(dave.getNumPixels()):
     pixel = dave.getPixel1D(i)
-    if pixel.getRed() > 200 and pixel.getGreen() > 200 \
-       and pixel.getBlue() > 200:
+    threshold = 200
+    if pixel.getRed() > threshold and \
+       pixel.getGreen() > threshold and \
+       pixel.getBlue() > threshold:
        dave.setPixel1D(i, Pixel(0,0,0))
 
 input("Look at me...")
