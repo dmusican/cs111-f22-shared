0a6d20ff2560000c5dd0b5a2dd8c6f1e7a57f76b
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-12 14:15:38 -0500

    Scripted auto-commit on change (2022-10-12 14:15:38) by gitwatch.sh

--- section-01-5a/day13/prog2.py
+++ section-01-5a/day13/prog2.py
@@ -2,14 +2,13 @@
 
 def removeBright(image):
 
-
-    for i in range(dave.getNumPixels()):
-        pixel = dave.getPixel1D(i)
+    for i in range(image.getNumPixels()):
+        pixel = image.getPixel1D(i)
         threshold = 150
         if pixel.getRed() > threshold and \
            pixel.getGreen() > threshold and \
            pixel.getBlue() > threshold:
-           dave.setPixel1D(i, dave.getPixel1D(i-1))
+           image.setPixel1D(i, image.getPixel1D(i-1))
 
 
 
