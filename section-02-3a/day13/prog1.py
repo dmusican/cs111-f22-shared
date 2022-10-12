7fe5e8168dcdf43cd56bdeb51dc72d3e5434790b
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-12 11:08:40 -0500

    Scripted auto-commit on change (2022-10-12 11:08:40) by gitwatch.sh

--- section-02-3a/day13/prog1.py
+++ section-02-3a/day13/prog1.py
@@ -1,7 +1,22 @@
+from images import *
 
-response = input("Favorite color?")
-# if response != "blue" and response != "green":
-# if not (response == blue)  and  not (response == green)
-# DeMorgan's Laws (one of them)
-if not (response == "blue" or response == "green"):
-    print("Me too!")
+win = ImageWin(480, 640, "Me!!!!!")
+dave = FileImage('dave-selfie.gif')
+dave.draw(win)
+
+for i in range(dave.getNumPixels()):
+    #print(dave.getPixel1D(i))
+
+    threshold = 150
+    pixel = dave.getPixel1D(i)
+    if pixel.getRed() > threshold and \
+       pixel.getGreen() > threshold and \
+       pixel.getBlue() > threshold:
+
+        #dave.setPixel1D(i, Pixel(0,0,0))
+        # replace with one pixel left
+        dave.setPixel1D(i, dave.getPixel1D(i-1))
+
+
+
+input("Waiting....")
