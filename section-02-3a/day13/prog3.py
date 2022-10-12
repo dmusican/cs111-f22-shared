bb698bab9dbd1c47fd7c9ed3b34a3c800a82962c
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-12 11:37:40 -0500

    Scripted auto-commit on change (2022-10-12 11:37:40) by gitwatch.sh

---
+++ section-02-3a/day13/prog3.py
@@ -0,0 +1,12 @@
+def main():
+    win1 = ImageWin(480, 640, "Original")
+    orig = FileImage("dave-selfie.gif")
+    orig.draw(win1)
+
+    noBright = removeBright(orig)
+    win2 = ImageWin(480, 640, "Fixed")
+    noBright.draw(win2)
+
+    input("Looking...")
+
+main()
