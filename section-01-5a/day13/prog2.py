2fa04e59ee5eed22fa8294f01c8bc3f39aafd46b
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-12 14:16:47 -0500

    Scripted auto-commit on change (2022-10-12 14:16:47) by gitwatch.sh

--- section-01-5a/day13/prog2.py
+++ section-01-5a/day13/prog2.py
@@ -19,7 +19,7 @@ def removeBright(image):
 
 def main():
     win1 = ImageWin(700, 500, "Original")
-    orig = FileImage('dave-selfie.jpg')
+    orig = FileImage('dave-selfie.gif')
     orig.draw(win1)
 
     noBright = removeBright(orig)
