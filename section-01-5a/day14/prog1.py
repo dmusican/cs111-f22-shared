21adb713e2cc3b0465a1522fa131079dcb4254bc
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-14 14:48:01 -0500

    Scripted auto-commit on change (2022-10-14 14:48:01) by gitwatch.sh

--- section-01-5a/day14/prog1.py
+++ section-01-5a/day14/prog1.py
@@ -5,6 +5,12 @@ def main():
     win1 = ImageWin(orig.getWidth(), orig.getHeight(),
                     "Original")
     orig.draw(win1)
+
+    updated = blur(orig, 2)
+    win2 = ImageWin(updated.getWidth(), updated.getHeight(),
+                    "Updated")
+    updated.draw(win2)
+
     input("Look at me...")
 
 if __name__=="__main__":
