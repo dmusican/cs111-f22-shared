7cd91068ccb46d456802eeb2216512815d35ec2e
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-14 12:17:16 -0500

    Scripted auto-commit on change (2022-10-14 12:17:16) by gitwatch.sh

--- section-02-3a/day14/prog1.py
+++ section-02-3a/day14/prog1.py
@@ -4,6 +4,7 @@ def main():
     orig = FileImage('dave-selfie.gif')
     win1 = ImageWin(orig.getWidth(), orig.getHeight(), "Dave")
     orig.draw(win1)
+    input("Look at me!")
 
 if __name__=="__main__":
     main()
