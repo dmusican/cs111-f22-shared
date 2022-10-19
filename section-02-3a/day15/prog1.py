a5387304b3028fb4b7dd16c43fcd85be92736962
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-19 11:25:12 -0500

    Scripted auto-commit on change (2022-10-19 11:25:12) by gitwatch.sh

---
+++ section-02-3a/day15/prog1.py
@@ -0,0 +1,15 @@
+
+def main():
+    orig = FileImage("dave-selfie.gif")
+    win1 = ImageWin(orig.getWidth(), orig.getHeight(),
+                    "Image Processing")
+    orig.draw(win1)
+
+    # 30<->200 is x range (cols), 100<->250 is y range (rows)
+    cropped = crop(orig, 30, 201, 100, 251)
+    win2 = ImageWin(cropped.getWidth(), cropped.getHeight().
+                    "Cropped!")
+    cropped.draw(win2)
+
+if __name__=="__main__":
+    main()
