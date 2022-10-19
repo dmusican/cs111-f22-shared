962e9dc8076f37b33ef6da2b1beebb7317e82abb
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-19 11:25:19 -0500

    Scripted auto-commit on change (2022-10-19 11:25:19) by gitwatch.sh

--- section-02-3a/day15/prog1.py
+++ section-02-3a/day15/prog1.py
@@ -7,7 +7,7 @@ def main():
 
     # 30<->200 is x range (cols), 100<->250 is y range (rows)
     cropped = crop(orig, 30, 201, 100, 251)
-    win2 = ImageWin(cropped.getWidth(), cropped.getHeight().
+    win2 = ImageWin(cropped.getWidth(), cropped.getHeight(),
                     "Cropped!")
     cropped.draw(win2)
 
