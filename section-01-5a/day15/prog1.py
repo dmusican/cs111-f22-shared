611e2083cdc81b5f3fd904ce1d771cc493014f3f
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-19 14:02:24 -0500

    Scripted auto-commit on change (2022-10-19 14:02:23) by gitwatch.sh

--- section-01-5a/day15/prog1.py
+++ section-01-5a/day15/prog1.py
@@ -1,5 +1,16 @@
 from images import *
 
+def blur(original, width):
+    newPic = original.copy()
+
+    for i in range(newPic.getNumPixels()):
+        newPixel = Pixel(average of red values to left and right,
+                         average of green values to left and right,
+                         average of blue values to left and right)
+        newPic.setPixel1D(i, newPixel)
+
+    return newPic
+
 def main():
     orig = FileImage('dave-selfie.gif')
     win1 = ImageWin(orig.getWidth(), orig.getHeight(), "Original")
