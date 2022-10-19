de984b578be48b3dc3be9a32c41ceb1aef6d6516
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-19 14:13:16 -0500

    Scripted auto-commit on change (2022-10-19 14:13:16) by gitwatch.sh

--- section-01-5a/day15/prog2.py
+++ section-01-5a/day15/prog2.py
@@ -3,12 +3,12 @@ from images import *
 def flip(original):
     newPic = original.copy()
 
+    for i in range(newPic.getWidth()):
+        for j in range(newPic.getHeight()):
+           oppositePixel = image.getPixel2D(i, image.getHeight() - j - 1)
+           newPic.setPixel2D(i, j, oppositePixel)
 
-
-
-
-
-    return newPic
+           return newPic
 
 def main():
     orig = FileImage('dave-selfie.gif')
