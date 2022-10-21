3643c596024142fabe063c16a4a66c2f0ab9e308
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-21 14:50:20 -0500

    Scripted auto-commit on change (2022-10-21 14:50:20) by gitwatch.sh

--- section-01-5a/day16/dice-example.py
+++ section-01-5a/day16/dice-example.py
@@ -10,17 +10,20 @@ class Die:
     def setShowing(self, valueToSet):
         self.showing = valueToSet
 
+    def setNumSides(self, sidesToSet):
+        self.numSides = sidesToSet
 
 def main():
     die1 = Die()
+    die1.setNumSides(6)
     die2 = Die()
+    die2.setNumSides(1000)
 
     die1.setShowing(3)
     print("Die 1 is", die1.getShowing())
     die1.roll()
     print("Die 1 is", die1.getShowing())
 
-    die2.setNumSides(1000)
     die2.setShowing(5)
     print("Die 2 is", die2.getShowing())
     print("Die 1 is", die1.getShowing())
