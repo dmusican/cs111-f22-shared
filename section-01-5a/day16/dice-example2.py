abe7dc60a8bb84bdccd73b7714481863a5304c68
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-21 15:08:28 -0500

    Scripted auto-commit on change (2022-10-21 15:08:28) by gitwatch.sh

---
+++ section-01-5a/day16/dice-example2.py
@@ -0,0 +1,36 @@
+import random
+
+class Die:
+    # I want a default value for numSides to be 6
+    # is it something like   default set numsides = 6? No.
+    def __init__(self):  # special built-in name which means run first
+        self.numSides = 6
+
+    def roll(self):
+        self.showing = random.randrange(1, self.numSides + 1)
+
+    def getShowing(self):
+        return self.showing
+
+    def setShowing(self, valueToSet):
+        self.showing = valueToSet
+
+    def setNumSides(self, sidesToSet):
+        self.numSides = sidesToSet
+
+def main():
+    die1 = Die()
+    die2 = Die()
+    die2.setNumSides(1000)
+
+    die1.setShowing(3)
+    print("Die 1 is", die1.getShowing())
+    die1.roll()
+    print("Die 1 is", die1.getShowing())
+
+    die2.roll()
+    print("Die 2 is", die2.getShowing())
+    print("Die 1 is", die1.getShowing())
+
+if __name__=="__main__":
+    main()
