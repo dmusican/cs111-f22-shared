dbfa758093b60ea8861ed9ceec3c52e4ad23ea7e
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-21 12:37:17 -0500

    Scripted auto-commit on change (2022-10-21 12:37:17) by gitwatch.sh

---
+++ section-02-3a/day16/dice-example2.py
@@ -0,0 +1,39 @@
+import random
+
+class Die:
+
+    #default value
+    # self.sides = 6, at first, when the object is
+    #    first created
+    # ???? initial default self.sides = 6
+    def __init__(self): # special name, means do first
+        self.sides = 6
+
+    def roll(self):
+        self.showing = random.randrange(1,
+                                        self.sides+1)
+
+    def getShowing(self):
+        return self.showing
+
+    def setShowing(self, valueToShow):
+        self.showing = valueToShow
+
+    def setSides(self, sidesToSet):
+        self.sides = sidesToSet
+
+def main():
+    die1 = Die()
+    #die1.setShowing(3)
+    #print(die1.getShowing())
+    die1.roll()
+    print("Die 1", die1.getShowing())
+
+    die2 = Die()
+    die2.setSides(1000)
+    die2.roll()
+    print("Die 2", die2.getShowing())
+    print("Die 1", die1.getShowing())
+
+if __name__=="__main__":
+    main()
