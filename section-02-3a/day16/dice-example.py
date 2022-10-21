56a65c61b8fd7b8c552a8c267253e7352203075c
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-21 12:16:04 -0500

    Scripted auto-commit on change (2022-10-21 12:16:04) by gitwatch.sh

--- section-02-3a/day16/dice-example.py
+++ section-02-3a/day16/dice-example.py
@@ -1,5 +1,5 @@
 class Die:
-    def roll(self):
+    #def roll(self):
         # Code goes here
 
     def getShowing(self):
@@ -13,8 +13,8 @@ def main():
     die1 = Die()
     die1.setShowing(3)
     print(die1.getShowing())
-    die1.roll()
-    print(die1.getShowing())
+    #die1.roll()
+    #print(die1.getShowing())
 
 if __name__=="__main__":
     main()
