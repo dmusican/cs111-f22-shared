f5123ce888f1387340fbf034bff240d09a7e07f7
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-24 14:49:31 -0500

    Scripted auto-commit on change (2022-10-24 14:49:31) by gitwatch.sh

--- section-01-5a/day17/fraction.py
+++ section-01-5a/day17/fraction.py
@@ -1,6 +1,16 @@
+class Fraction:
+
+    def __init__(self, topToSet, bottomToSet):
+        self.top = topToSet
+        self.bottom = bottomToSet
+
+
+    def display(self, .....):
+
+
 def main():
-    frac1 = Fraction(3, 5)
-    frac2 = Fraction(1, 3)
+    frac1 = Fraction(3, 5)   # 3/5
+    frac2 = Fraction(1, 3)   # 1/3
 
     frac1.display()
     frac2.display()
