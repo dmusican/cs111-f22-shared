0fc7325f598469443f77ab9bd4d91b079eb05500
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-24 11:55:49 -0500

    Scripted auto-commit on change (2022-10-24 11:55:49) by gitwatch.sh

--- section-02-3a/day17/fraction.py
+++ section-02-3a/day17/fraction.py
@@ -1,6 +1,17 @@
+class Fraction:
+    def __init__(self, topToSet, bottomToSet):
+        self.top = topToSet
+        self.bottom = bottomToSet
+
+    def display(self):
+        print(self.top, "/", self.bottom)
+
 def main():
     frac1 = Fraction(3,5)
     frac2 = Fraction(2,7)
+
+    frac1.display()
+    frac2.display()
 
 if __name__=="__main__":
     main()
