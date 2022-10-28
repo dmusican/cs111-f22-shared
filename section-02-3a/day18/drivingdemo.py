8e8f4ecd65f49aacc9063684eedc8859d8227f04
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-28 12:04:42 -0500

    Scripted auto-commit on change (2022-10-28 12:04:42) by gitwatch.sh

--- section-02-3a/day18/drivingdemo.py
+++ section-02-3a/day18/drivingdemo.py
@@ -4,6 +4,7 @@ def main():
     road = Rectangle(Point(0, 200), Point(499, 400))
     road.setFill('darkgray')
     road.draw(win)
+    input("Look at me!!!!")
 
 if __name__=="__main__":
     main()
