e933923472e330fe0b42d236785979efc6c7506e
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-28 14:56:37 -0500

    Scripted auto-commit on change (2022-10-28 14:56:37) by gitwatch.sh

--- section-01-5a/day18/grid.py
+++ section-01-5a/day18/grid.py
@@ -3,4 +3,7 @@ board = []
 for i in range(5):
     board.append(['-', '-', '-'])
 
-print(board)
+#print(board)
+# Print this out in a much prettier way
+for i in range(len(board)):
+    print(board[i])
