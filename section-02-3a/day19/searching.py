b5d2c0ee665622def8279c6a8be8a3e810eb70ec
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-10-31 11:19:43 -0500

    Scripted auto-commit on change (2022-10-31 11:19:43) by gitwatch.sh

--- section-02-3a/day19/searching.py
+++ section-02-3a/day19/searching.py
@@ -1,3 +1,10 @@
+def linearSearch(items, target):
+    for i in range(len(items)):
+        if items[i] == target:
+            return i  # location where found
+
+    return -1   # not found
+
 words = []
 inpfile = open('twl06clean.txt', 'r')
 for word in inpfile:
