2a2ef5d2f194dbf99cf78a21debf4a43afbb96f8
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-11-02 11:46:07 -0500

    Scripted auto-commit on change (2022-11-02 11:46:07) by gitwatch.sh

--- section-02-3a/day20/recursion.py
+++ section-02-3a/day20/recursion.py
@@ -25,4 +25,12 @@ def sumup(items):
         return items[0] + sumup(items[1:])
 
 
-print(sumup([9, 2, 4, 3]))
+#print(sumup([9, 2, 4, 3]))
+
+# Check if a list is sorted. Return True if
+# so, False if not
+def checkSorted(items):
+    if len(items) == 1:
+        return True
+    else:
+        return checkSorted(items[1:]) and items[0] < items[1]
