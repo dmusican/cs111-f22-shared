e90ec41bd9f99c1dc9ed57b5c50e8b38625374f8
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-11-04 14:41:58 -0500

    Scripted auto-commit on change (2022-11-04 14:41:58) by gitwatch.sh

--- section-01-5a/day21/sorts.py
+++ section-01-5a/day21/sorts.py
@@ -1,12 +1,29 @@
 import random
 
+def selectionSort(items):
+    # i is the position we will swap into
+    for i in range(len(items)):
+        # Find the smallest value in the remainder of
+        # list starting at position i (and where it is)
+        smallestPos = i
+        for j in range(i+1, len(items)):
+            if items[j] < items[smallestPos]:
+                smallestPos = j
+        # Swap items at locations i and smallestPos
+        # (x, y) = (y, x)
+        (items[i], items[smallestPos]) = \
+            (items[smallestPost], items[i])
+
+
+
 
 def shuffle(items):
     random.seed(55057)
     for i in range(1000000):
         spot1 = random.randrange(len(items))
         spot2 = random.randrange(len(items))
-        (items[spot1], items[spot2]) = (items[spot2], items[spot1])
+        (items[spot1], items[spot2]) = \
+            (items[spot2], items[spot1])
 
 def main():
     random.seed(90125)
