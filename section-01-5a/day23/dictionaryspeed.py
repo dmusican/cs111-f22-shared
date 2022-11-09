08d7961514b00bc77f61d826851fb8a5af76fb4e
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-11-09 14:26:27 -0600

    Scripted auto-commit on change (2022-11-09 14:26:27) by gitwatch.sh

---
+++ section-01-5a/day23/dictionaryspeed.py
@@ -0,0 +1,24 @@
+
+size = 2000
+
+print("Trying dictionary")
+itemsdict = {}
+for i in range(size):
+    itemsdict[i] = i
+
+matches = 0
+for i in range(size):
+    if i in dict:     # if it is a key
+        matches += 1
+print(matches)
+
+print("Trying list")
+itemslist = []
+for i in range(size):
+    itemslist.append(i)
+
+matches = 0
+for i in range(size):
+    if i in itemslist:
+        matches += 1
+print(matches)
