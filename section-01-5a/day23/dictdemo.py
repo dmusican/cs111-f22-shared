e959754cb49ab8d97fb1cc0fa85a046a64246142
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-11-09 14:18:27 -0600

    Scripted auto-commit on change (2022-11-09 14:18:27) by gitwatch.sh

--- section-01-5a/day23/dictdemo.py
+++ section-01-5a/day23/dictdemo.py
@@ -3,6 +3,6 @@ onecard = {'dmusicant': 4.32, 'abyerly': 0.67, 'wshakesp': 14.00}
 print(onecard['dmusicant'])
 print(onecard['wshakesp'])
 onecard['schiller'] = 98.32
-print(onecard)
 onecard['dmusicant'] = 50.19
+print(onecard)
 print(onecard['dmusicant'])
