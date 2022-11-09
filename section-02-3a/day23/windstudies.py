d5039577223e0830e93ea4c89cf9227a5dd6c0aa
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-11-09 11:51:11 -0600

    Scripted auto-commit on change (2022-11-09 11:51:11) by gitwatch.sh

--- section-02-3a/day23/windstudies.py
+++ section-02-3a/day23/windstudies.py
@@ -8,7 +8,13 @@ countByState = {}   # state: count
 
 for row in datareader:
     #print(row)
-    print(row['t_state'], row['t_county'])
-    print()
+    #print(row['t_state'], row['t_county'])
+    state = row['t_state']
+    if state not in countByState:
+        countByState[state] = 1
+    else:
+        countByState[state] += 1
 
 csvfile.close()
+
+print(countByState)
