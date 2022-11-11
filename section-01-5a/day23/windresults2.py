f3143bfe958229e7b73f985a4763057123c388d5
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-11-11 14:16:05 -0600

    Scripted auto-commit on change (2022-11-11 14:16:05) by gitwatch.sh

---
+++ section-01-5a/day23/windresults2.py
@@ -0,0 +1,18 @@
+import csv
+
+csvfile = open('wind_turbines.csv', 'r')
+datareader = csv.DictReader(csvfile)
+
+# Find how many wind turbines there are in each state
+countByState = {}
+
+for row in datareader:
+    # row... is a DICTIONARY!!!!!
+    state = row['t_state']
+    if state not in countByState:
+        countByState[state] = 0
+
+    countByState[state] += 1
+    # countByState[state] = countByState[state] + 1
+
+print(countByState)
