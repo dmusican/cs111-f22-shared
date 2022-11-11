44dc7a7d607e1004ac79e6cd0e4abc788bc1190b
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-11-11 12:15:16 -0600

    Scripted auto-commit on change (2022-11-11 12:15:16) by gitwatch.sh

--- section-02-3a/day23/windstudies2.py
+++ section-02-3a/day23/windstudies2.py
@@ -3,28 +3,16 @@ import csv
 csvfile = open('wind_turbines.csv', 'r')
 datareader = csv.DictReader(csvfile)
 
+# Count number of wind turbines in each state
+countByState = {}   # state: count
 
-for cat in datareader:
-    print(cat)
-    break
+for row in datareader:
+    state = row['t_state']
+    if state not in countByState:
+        countByState[state] = 0
+    countByState[state] += 1
 
+csvfile.close()
 
-
-
-
-
-
-
-# # Count number of wind turbines in each state
-# countByState = {}   # state: count
-
-# for row in datareader:
-#     state = row['t_state']
-#     if state not in countByState:
-#         countByState[state] = 0
-#     countByState[state] += 1
-
-# csvfile.close()
-
-# for state in countByState:
-#     print(state, countByState[state])
+for state in countByState:
+    print(state, countByState[state])
