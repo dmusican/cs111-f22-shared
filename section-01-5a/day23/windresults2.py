5563dc014e8adbfe2c7ffd5ef7538dd6ce7321ed
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-11-11 14:47:20 -0600

    Scripted auto-commit on change (2022-11-11 14:47:20) by gitwatch.sh

--- section-01-5a/day23/windresults2.py
+++ section-01-5a/day23/windresults2.py
@@ -7,8 +7,6 @@ datareader = csv.DictReader(csvfile)
 countByState = {}
 
 for row in datareader:
-    print(row)
-    print(type(row['faa_ors']))
     state = row['t_state']
     if state not in countByState:
         countByState[state] = 0
@@ -16,6 +14,5 @@ for row in datareader:
     countByState[state] += 1
     # countByState[state] = countByState[state] + 1
 
-    break
 
 #print(countByState)
