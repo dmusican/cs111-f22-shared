2e919eaf37efc5df0584ff171e5e75022690088f
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-11-14 13:58:14 -0600

    Scripted auto-commit on change (2022-11-14 13:58:14) by gitwatch.sh

--- section-01-5a/day23/windresults3.py
+++ section-01-5a/day23/windresults3.py
@@ -1,9 +1,13 @@
 import csv
 
 # Figure out average year that wind turbines are built per state
+yearsByState = {}
 with open('wind_turbines.csv', 'r') as csvfile:
     datareader = csv.DictReader(csvfile)
     for row in datareader:
         state = row['t_state']
         year = row['p_year']
-        print(state, year)
+        if state not in yearsByState:
+            yearsByState[state] = []
+
+        yearsByState[state].append(year)
