3a67d3992613514c6daac172e6abc83c564195b4
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-11-14 13:56:23 -0600

    Scripted auto-commit on change (2022-11-14 13:56:23) by gitwatch.sh

--- section-01-5a/day23/windresults3.py
+++ section-01-5a/day23/windresults3.py
@@ -8,8 +8,6 @@ with open('wind_turbines.csv', 'r') as csvfile:
         year = row['p_year']
         #print(state, year)
 
-
-with open('wind_turbines.csv', 'r') as csvfile:
     datareader = csv.DictReader(csvfile)
     for row in datareader:
         state = row['t_state']
