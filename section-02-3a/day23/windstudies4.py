d88c463cbdc3b3ac163fb46b4c06c8de3ba3753c
Dave Musicant / thinkpad <dmusicant+thinkpad@carleton.edu>
2022-11-14 11:17:39 -0600

    Scripted auto-commit on change (2022-11-14 11:17:39) by gitwatch.sh

--- section-02-3a/day23/windstudies4.py
+++ section-02-3a/day23/windstudies4.py
@@ -1,6 +1,7 @@
 import csv
 
 # Find average year of wind turbine construction per state
+yearByState = {}
 with open('wind_turbines.csv', 'r') as csvfile:
     datareader = csv.DictReader(csvfile)
 
